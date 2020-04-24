7a6aa90fe2cb4cffa04008b6ccf5e7b3769353ef
misroka <misroka@microsoft.com>
2020-04-24 22:24:13 +0200

    Add python code to read temperature sensor once

---
+++ temperature/make_one_reading.py
@@ -0,0 +1,8 @@
+from w1thermsensor import W1ThermSensor
+
+sensor = W1ThermSensor()
+
+temp = sensor.get_temperature()
+
+print("Hello Margotku. The temperature is: "+str(temp) + " Have a great day ;) ")
+
